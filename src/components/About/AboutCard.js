5b13372f6563e37038b7a2f6a015943d83bf9367
王晓海 <xiaohaiwang17@gmail.com>
2025-03-03 17:41:34 -0500

    update

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,15 +8,10 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Xiaohai Wang</span>,
-            a passionate <span className="purple">Cybersecurity Analyst</span> 
-            from <span className="purple">Guelph, Ontario</span>.
+            Hi Everyone, I am <span className="purple">Xiaohai Wang</span>, a passionate <span className="purple">Cybersecurity Analyst</span> from <span className="purple">Guelph, Ontario</span>.
             <br />
             <br />
-            I recently graduated with a <strong>Master’s degree in Cybersecurity and Threat Intelligence</strong> 
-            from the University of Guelph. I have over <strong>three years of hands-on experience</strong> 
-            in <span className="purple">penetration testing</span>, <span className="purple">cyber threat intelligence (CTI)</span>, 
-            and <span className="purple">malware analysis</span>.
+            I recently graduated with a <strong>Master’s degree in Cybersecurity and Threat Intelligence</strong> from the University of Guelph. I have over <strong>three years of hands-on experience</strong> in <span className="purple">penetration testing</span>, <span className="purple">cyber threat intelligence (CTI)</span>, and <span className="purple">malware analysis</span>.
             <br />
             <br />
             My expertise lies in:
@@ -25,8 +20,7 @@ function AboutCard() {
               <li className="about-activity"><ImPointRight /> Code auditing & security consulting</li>
               <li className="about-activity"><ImPointRight /> Threat intelligence and malware analysis</li>
             </ul>
-            I am dedicated to <strong>identifying and mitigating digital threats</strong>, improving security infrastructure, 
-            and sharing cybersecurity knowledge through <span className="purple">technical writeups</span> and 
+            I am dedicated to <strong>identifying and mitigating digital threats</strong>, improving security infrastructure, and sharing cybersecurity knowledge through <span className="purple">technical writeups</span> and
             <span className="purple"> open-source contributions</span>.
           </p>
 
